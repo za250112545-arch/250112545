29a38d9e776f1d0021d26d48b481b3181c60b01f
za250112545-arch <za250112545@zapopan.tecmm.edu.mx>
2026-03-27 09:01:29 -0600

    Update DataSet.cpp

--- Assignment2/DataSet.cpp
+++ Assignment2/DataSet.cpp
@@ -1,16 +1,17 @@
 #include "DataSet.hpp"
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 using namespace std;
 
 DataSet::DataSet() {
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 10); // valores entre 1 y 10
-
-    for (int i = 0; i < 20; i++) 
+    uniform_int_distribution<> dist(50, 80); // valores entre 1 y 10
+    for (int i = 0; i < 40; i++) 
         data.push_back(dist(gen));
+    std::sort(data.begin(),data.end());
 }
 
 DataSet::DataSet(const vector<int>& values) : data(values) {
@@ -26,7 +27,6 @@ void DataSet::setData(const vector<int>& values) {
 vector<int> DataSet::getData() const {
     return data;
 }
-
 void DataSet::printData() const {
     cout << "Muestra de datos: ";
     for (size_t i = 0; i < data.size(); i++) 
